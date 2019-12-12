90b6d830f900fc1772dfc394e3ff726a2c5cf065
Mathyas Giudici <mathyas.giudici@mail.polimi.it>
2019-12-12 11:46:22 +0100

    New bg @ corsi + panel

--- Assets/scripts/MagicOrchestraScripts/MagicOrchestraUtils.cs
+++ Assets/scripts/MagicOrchestraScripts/MagicOrchestraUtils.cs
@@ -7,7 +7,7 @@ public static class MagicOrchestraUtils
     public const string pathToCorsiSequences = "/corsi_sequences.csv";
     public const string pathToGame2Sequence = "/digitspan_sequences.csv";
 
-    public const string backgroundColor = "56122A";
+    public const string backgroundColor = "93373F";
     public const string primaryColor = "F0A84D";
     public const string secondaryColor = "FFFFFF" ;
 
