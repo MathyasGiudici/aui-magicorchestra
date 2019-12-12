1d81dc3d5db37c5fa70278f1a529164a95ceaf7c
Mathyas Giudici <mathyas.giudici@mail.polimi.it>
2019-12-12 14:14:51 +0100

    New UI in the games

---
+++ Assets/scripts/MagicOrchestraScripts/GeneralController/Game3/GestureToggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureToggle : MonoBehaviour
+{
+	public GameObject timePanel;
+
+    void Start()
+    {
+        timePanel.SetActive(false);
+    }
+
+    public void ChangeTimeZenithPanel()
+    {
+        timePanel.SetActive(!gameObject.GetComponent<GeneralGameToggle>().GetToggleStatus());
+    }
+}
