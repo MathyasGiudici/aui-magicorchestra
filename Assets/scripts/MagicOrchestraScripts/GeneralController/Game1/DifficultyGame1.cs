3112d85668599bb020a946b2ca987c3c10a63736
Mathyas Giudici <mathyas.giudici@mail.polimi.it>
2020-01-31 00:22:36 +0100

    Game settings panels fixed

---
+++ Assets/scripts/MagicOrchestraScripts/GeneralController/Game1/DifficultyGame1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyGame1 : MonoBehaviour
+{
+    // Internal value of difficulty
+    private int currentDifficulty = 2;
+
+    // Difficulty values setting from Unity
+    public int lowDifficulty = 2;
+    public int highDifficulty = 12;
+
+    /* <summary>
+     * Update is called once per frame
+     * </summary>
+     */
+    void Update()
+    {
+        float value = gameObject.GetComponent<Slider>().value;
+        float newValue = (float)Mathf.Round(value * 10f) / 10f;
+        gameObject.GetComponent<Slider>().value = newValue;
+
+        // Ranging the value
+        newValue *= (highDifficulty - lowDifficulty);
+        newValue = Mathf.RoundToInt(newValue) + lowDifficulty;
+
+        int correct = (int)newValue;
+
+        if (correct == 5 || correct == 9)
+            correct++;
+
+        this.currentDifficulty = correct;
+        gameObject.transform.GetChild(gameObject.transform.childCount - 1).gameObject.GetComponent<Text>().text = correct.ToString();
+
+    }
+
+    public int GetCurrentDifficulty()
+    {
+        return this.currentDifficulty;
+    }
+}
--- Assets/scripts/MagicOrchestraScripts/GeneralController/Utils/GeneralGameToggle.cs
+++ Assets/scripts/MagicOrchestraScripts/GeneralController/Utils/GeneralGameToggle.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GeneralGameToggle : MonoBehaviour
 {
     // Internal status
-	private bool status = true;
+	private bool status = false;
 
-    public void ChangeStatus()
+    public void Update()
 	{
-		this.status = !this.status;
+        this.status = gameObject.GetComponent<Toggle>().isOn;
 	}
 
     public bool GetToggleStatus()
