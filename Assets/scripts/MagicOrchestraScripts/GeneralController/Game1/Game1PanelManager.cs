db524ff4adf11f3f71194c1dfe0d2bed3e7db6ce
Mathyas Giudici <mathyas.giudici@mail.polimi.it>
2020-01-31 10:28:19 +0100

    Fixed error in MagicOrchestra

--- Assets/scripts/MagicOrchestraScripts/GeneralController/Game1/Game1PanelManager.cs
+++ Assets/scripts/MagicOrchestraScripts/GeneralController/Game1/Game1PanelManager.cs
@@ -7,10 +7,8 @@ public class Game1PanelManager : MonoBehaviour
 {
 	public void GamePressed()
 	{
-		Game1Parameters.Difficulty = this.gameObject.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<DifficultySlider>().GetCurrentDifficulty();
+		Game1Parameters.Difficulty = this.gameObject.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<DifficultyGame1>().GetCurrentDifficulty();
 		Game1Parameters.TimeInShowing = this.gameObject.transform.GetChild(2).GetChild(0).GetChild(1).GetComponent<TimeSlider>().GetCurrentTime();
-
-		Game1Parameters.LogMe();
 
 		SceneManager.LoadScene("InstruMaps");
 	}
